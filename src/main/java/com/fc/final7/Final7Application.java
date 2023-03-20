c16773817e832ce2a446842ade7bf95cc7ca79c2
CBO <quddhr9523@naver.com>
2023-03-20 17:45:32 +0900

    feat: JPA auditing 사용시 필요한 어노테이션 설정

--- src/main/java/com/fc/final7/Final7Application.java
+++ src/main/java/com/fc/final7/Final7Application.java
@@ -2,8 +2,10 @@ package com.fc.final7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Final7Application {
 
 	public static void main(String[] args) {
