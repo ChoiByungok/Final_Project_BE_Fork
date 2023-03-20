95ee3404708a1f3e5c0173209bba7d9108d253d2
Darby <lkh55913@naver.com>
2023-03-20 13:56:27 +0900

    Config #4: Create default spring boot project
    Spring Boot 프로젝트 설정

---
+++ src/test/java/com/fc/final7/Final7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.final7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
