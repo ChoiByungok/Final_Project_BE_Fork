ecd698e78f51a271f5a093bd912bd10e84e2b960
CBO <quddhr9523@naver.com>
2023-03-21 17:05:40 +0900

    feat: reservation 패키지 엔티티 초기설정

--- src/main/java/com/fc/final7/domain/reservation/entity/Reservation.java
+++ src/main/java/com/fc/final7/domain/reservation/entity/Reservation.java
@@ -1,4 +1,52 @@
 package com.fc.final7.domain.reservation.entity;
 
-public class Reservation {
+import com.fc.final7.domain.member.entity.Member;
+import com.fc.final7.domain.product.entity.Product;
+import com.fc.final7.domain.product.entity.ProductPeriod;
+import com.fc.final7.global.entity.Auditing;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "reservation")
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Reservation extends Auditing {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "product_period_id")
+    private ProductPeriod productPeriod;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "status", columnDefinition = "VARCHAR(20)")
+    @Enumerated(STRING)
+    private Status status;
+
+    @Column(name = "email", columnDefinition = "VARCHAR(255)")
+    private String email;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(20)")
+    private String name;
+
+    @Column(name = "phone", columnDefinition = "VARCHAR(40)")
+    private String phone;
+
+    @Column(name = "notice", columnDefinition = "TINYINT")
+    private Boolean notice;
 }
