991742aca7cb8b4c44a3b8ef2dc78dc5dcb0f58c
CBO <quddhr9523@naver.com>
2023-03-21 17:06:43 +0900

    feat: review 패키지 엔티티 초기설정

--- src/main/java/com/fc/final7/domain/review/entity/Review.java
+++ src/main/java/com/fc/final7/domain/review/entity/Review.java
@@ -1,4 +1,59 @@
 package com.fc.final7.domain.review.entity;
 
-public class Review {
+import com.fc.final7.domain.comment.entity.Comment;
+import com.fc.final7.domain.product.entity.Product;
+import com.fc.final7.global.entity.Auditing;
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.fc.final7.domain.review.entity.Posting.POSTING;
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "review")
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Review extends Auditing {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @OneToMany(mappedBy = "review")
+    private List<ReviewContent> reviewContents = new ArrayList<>();
+
+    @OneToMany(mappedBy = "review")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "thumbnail", columnDefinition = "TEXT")
+    private String thumbnail;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(20)")
+    private String name;
+
+    @Column(name = "view_count")
+    private Long viewCount;
+
+    @Column(name = "password", columnDefinition = "VARCHAR(255)")
+    private String password;
+
+    @Builder.Default
+    @Column(name = "status", columnDefinition = "VARCHAR(10) DEFAULT 'POSTING'")
+    @Enumerated(STRING)
+    private Posting posting = POSTING;
+
 }
--- src/main/java/com/fc/final7/domain/review/entity/ReviewContent.java
+++ src/main/java/com/fc/final7/domain/review/entity/ReviewContent.java
@@ -1,4 +1,39 @@
 package com.fc.final7.domain.review.entity;
 
+import com.fc.final7.global.entity.ContentType;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Table(name = "review_content")
+@Builder
 public class ReviewContent {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "review_content_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(name = "priority")
+    private Long priority;
+
+    @Column(name = "type", columnDefinition = "VARCHAR(20)")
+    @Enumerated(STRING)
+    private ContentType contentType;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
 }
