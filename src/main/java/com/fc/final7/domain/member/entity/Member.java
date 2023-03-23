056b80b12075eaba4f71ccd2c5d5d2e933b84442
Chaseungjun <chas369@naver.com>
2023-03-23 16:02:40 +0900

    #9 이메일과 패스워드를 검증하는 로직을 추가, 회원가입에 성공했을 때 Member를 생성하는 로직 추가

--- src/main/java/com/fc/final7/domain/member/entity/Member.java
+++ src/main/java/com/fc/final7/domain/member/entity/Member.java
@@ -1,19 +1,24 @@
 package com.fc.final7.domain.member.entity;
 
+import com.fc.final7.domain.member.dto.SignUpRequestDto;
+import com.fc.final7.domain.member.enums.Gender;
+import com.fc.final7.domain.member.enums.IsMember;
+import com.fc.final7.domain.member.enums.Role;
 import com.fc.final7.domain.reservation.entity.Reservation;
 import com.fc.final7.global.entity.Auditing;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
-import static com.fc.final7.domain.member.entity.IsMember.YES;
+import static com.fc.final7.domain.member.enums.IsMember.YES;
 import static javax.persistence.EnumType.STRING;
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -37,10 +42,10 @@ public class Member extends Auditing {
     @OneToMany(mappedBy = "member")
     private List<Reservation> reservations = new ArrayList<>();
 
-    // TODO: 2023-03-21 ref 멤버는 어떻게 정해야 할지 몰라서 냅둠
-
+    @Builder.Default
     @Column(name = "role", columnDefinition = "VARCHAR(64)")
-    private String role;
+    @Enumerated(STRING)
+    private Role role = Role.MEMBER;
 
     @Column(name = "email",columnDefinition = "VARCHAR(255)", unique = true)
     private String email;
@@ -80,4 +85,37 @@ public class Member extends Auditing {
     @Column(name = "status", columnDefinition = "VARCHAR(10) DEFAULT 'YES'")
     @Enumerated(STRING)
     private IsMember isMember = YES;
+
+
+    public static Member createMember(SignUpRequestDto memberDto, PasswordEncoder encoder) throws Exception {
+        if (validationEmail(memberDto) == false) {
+            throw new Exception("이메일 형식으로 입력해주세요");
+        }
+        if (validationPassword(memberDto) == false) {
+            throw new Exception("8~16자의 비밀번호 형식으로 입력해주세요");
+        }
+       return Member.builder()
+                .email(memberDto.getEmail())
+                .password(encoder.encode(memberDto.getPassword()))
+                .name(memberDto.getName())
+                .age(memberDto.getAge())
+                .gender(memberDto.getGender())
+                .birth(memberDto.getBirth())
+                .phone(memberDto.getPhone())
+                .build();
+    }
+
+
+    public static final Pattern isEmail = Pattern.compile("^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
+
+    public static final Pattern isPassword = Pattern.compile("^.{8,16}$");
+
+    public static boolean validationEmail(SignUpRequestDto dto) {
+        return isEmail.matcher(dto.getEmail()).matches();
+    }
+
+    public static boolean validationPassword(SignUpRequestDto dto) {
+        return isPassword.matcher(dto.getPassword()).matches();
+    }
+
 }
