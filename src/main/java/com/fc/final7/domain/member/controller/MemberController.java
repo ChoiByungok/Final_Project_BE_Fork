5cf9997eefdab36bfa1fb6483b63267f9dbaade3
Chaseungjun <chas369@naver.com>
2023-03-26 19:14:59 +0900

    #14 아이디 중복 체크와 로그인 코드 추가

--- src/main/java/com/fc/final7/domain/member/controller/MemberController.java
+++ src/main/java/com/fc/final7/domain/member/controller/MemberController.java
@@ -1,11 +1,11 @@
 package com.fc.final7.domain.member.controller;
 
+import com.fc.final7.domain.member.dto.MemberLoginDto;
+import com.fc.final7.domain.member.dto.MemberResponseDto;
 import com.fc.final7.domain.member.dto.SignUpRequestDto;
-import com.fc.final7.domain.member.dto.SignupResponseDto;
 import com.fc.final7.domain.member.service.MemberService;
 import com.fc.final7.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +19,19 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signUp")
-    public BaseResponse<SignupResponseDto> signUp(@RequestBody @Valid SignUpRequestDto requestDto) throws Exception {
-        SignupResponseDto signupResponseDto = memberService.signUp(requestDto);
-        return BaseResponse.of(1, "회원가입이 완료되었습니다.", signupResponseDto);
+    public BaseResponse<MemberResponseDto> signUp(@RequestBody @Valid SignUpRequestDto requestDto) throws Exception {
+        MemberResponseDto memberResponseDto = memberService.signUp(requestDto);
+        return BaseResponse.of(1, "회원가입이 완료되었습니다.", memberResponseDto);
+    }
+
+    @PostMapping("/signUp/checkId")
+    public boolean checkDuplicationEmail(@RequestBody SignUpRequestDto requestDto) {
+       return memberService.checkDuplicationEmail(requestDto);
+    }
+
+    @PostMapping("/login")
+    public BaseResponse<MemberResponseDto> login(@RequestBody MemberLoginDto loginDto){
+        MemberResponseDto memberResponseDto = memberService.login(loginDto);
+        return BaseResponse.of(1, "로그인에 성공했습니다", memberResponseDto);
     }
 }
