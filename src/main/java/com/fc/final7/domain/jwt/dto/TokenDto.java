835606f8b60e5bb8db00c9d28c0e1b05de08b48c
Chaseungjun <chas369@naver.com>
2023-03-26 19:17:08 +0900

    #14 엑세스 토큰과 리프레쉬 토큰을 만들어서 사용자에게 넘겨주는 클래스 생성

---
+++ src/main/java/com/fc/final7/domain/jwt/dto/TokenDto.java
@@ -0,0 +1,27 @@
+package com.fc.final7.domain.jwt.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class TokenDto {
+
+    // 엑세스 토큰과 리프레쉬 토큰을 만들어서 사용자에게 넘겨주는 클래스
+    private String accessToken;
+    private String refreshToken;
+    private String accessStartTime;
+    private String accessExpirationTime;
+    private String refreshExpirationTime;
+
+    public TokenDto(String accessToken, String refreshToken, String accessStartTime, String accessExpirationTime, String refreshExpirationTime) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.accessStartTime = accessStartTime;
+        this.accessExpirationTime = accessExpirationTime;
+        this.refreshExpirationTime = refreshExpirationTime;
+    }
+
+}
