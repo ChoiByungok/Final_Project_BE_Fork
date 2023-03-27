d9f6b3282d20a85c2db1f8693f81ac72113375bc
CBO <quddhr9523@naver.com>
2023-03-27 16:50:25 +0900

    rename: responseDTO 라고 명시적으로 바꿈

---
+++ src/main/java/com/fc/final7/domain/product/dto/ProductResponseDTO.java
@@ -0,0 +1,28 @@
+package com.fc.final7.domain.product.dto;
+
+import com.fc.final7.domain.product.entity.Product;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class ProductDTO {
+
+    private Long productId;
+    private String thumbnail;
+    private String productName;
+    private Long productPrice;
+    private String briefExplanation;
+    private Integer period;
+
+    public ProductDTO(Product product) {
+        productId = product.getId();
+        thumbnail = product.getThumbnail();
+        productName = product.getTitle();
+        productPrice = product.getPrice();
+        briefExplanation = product.getDescription();
+        period = product.getTerm();
+    }
+}
