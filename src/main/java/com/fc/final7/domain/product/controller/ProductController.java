02f7b67b9b2ef653c3d2b168e6e2e7932a998096
CBO <quddhr9523@naver.com>
2023-03-27 19:05:53 +0900

    feat: productController 개발

--- src/main/java/com/fc/final7/domain/product/controller/ProductController.java
+++ src/main/java/com/fc/final7/domain/product/controller/ProductController.java
@@ -1,9 +1,26 @@
 package com.fc.final7.domain.product.controller;
 
+import com.fc.final7.domain.product.dto.ProductPagingDTO;
+import com.fc.final7.domain.product.dto.SearchConditionListDTO;
+import com.fc.final7.domain.product.service.ProductService;
+import com.fc.final7.global.response.GlobalSuccessDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+
+    @PostMapping("/products")
+    private GlobalSuccessDTO<ProductPagingDTO> test(@RequestBody SearchConditionListDTO searchConditionDTOS) {
+        PageRequest pageRequest = PageRequest.of(searchConditionDTOS.getPage(), 12);
+        ProductPagingDTO productPagingDTO = productService.groupByCategory(searchConditionDTOS, pageRequest);
+        return new GlobalSuccessDTO<>(productPagingDTO.getProducts().size(), "성공", productPagingDTO);
+    }
 }
