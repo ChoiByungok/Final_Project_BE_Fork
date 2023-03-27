ad798578d605cf4aac84ae405dcaa6ca182a91c6
CBO <quddhr9523@naver.com>
2023-03-27 16:58:38 +0900

    JpaQueryFactory spring bean 등록

--- src/main/java/com/fc/final7/Final7Application.java
+++ src/main/java/com/fc/final7/Final7Application.java
@@ -1,8 +1,12 @@
 package com.fc.final7;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +14,12 @@ public class Final7Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Final7Application.class, args);
+	}
+
+	//queryDSL 빈등록
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
 	}
 
 }
