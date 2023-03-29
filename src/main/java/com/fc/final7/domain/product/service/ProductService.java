697c46b2308a1b0d18447a33ea41cdde5eaafe95
CBO <quddhr9523@naver.com>
2023-03-30 01:16:07 +0900

    feat: 상품 상세조회 관련 서비스계층 구현

--- src/main/java/com/fc/final7/domain/product/service/ProductService.java
+++ src/main/java/com/fc/final7/domain/product/service/ProductService.java
@@ -2,10 +2,17 @@ package com.fc.final7.domain.product.service;
 
 import com.fc.final7.domain.product.dto.response.ProductPagingDTO;
 import com.fc.final7.domain.product.dto.request.SearchConditionListDTO;
+import com.fc.final7.domain.product.dto.response.detail.ProductDetailResponseDTO;
+import com.fc.final7.domain.product.dto.response.detail.ReviewResponseDTO;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ProductService {
 
     public ProductPagingDTO groupByCategory(SearchConditionListDTO searchConditionListDTO, Pageable pageable);
 
+    ProductDetailResponseDTO selectProductDetail(Long productId);
+
+    List<ReviewResponseDTO> selectProductDetailInReviews (Long productId, Pageable pageable);
 }
--- src/main/java/com/fc/final7/domain/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/fc/final7/domain/product/service/impl/ProductServiceImpl.java
@@ -3,9 +3,14 @@ package com.fc.final7.domain.product.service.impl;
 import com.fc.final7.domain.product.dto.response.ProductPagingDTO;
 import com.fc.final7.domain.product.dto.response.ProductResponseDTO;
 import com.fc.final7.domain.product.dto.request.SearchConditionListDTO;
+import com.fc.final7.domain.product.dto.response.detail.ProductDetailResponseDTO;
+import com.fc.final7.domain.product.dto.response.detail.ReviewResponseDTO;
 import com.fc.final7.domain.product.entity.Product;
 import com.fc.final7.domain.product.repository.datajpa.CategoryRepository;
+import com.fc.final7.domain.product.repository.datajpa.ProductRepository;
 import com.fc.final7.domain.product.service.ProductService;
+import com.fc.final7.domain.review.repository.ReviewRepository;
+import com.fc.final7.global.exception.NoSearchProductException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +24,12 @@ import java.util.stream.Collectors;
 public class ProductServiceImpl implements ProductService{
 
     private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+    private final ReviewRepository reviewRepository;
 
+
+
+    //그룹별 테마별 지역별 카테고리를 동적으로 받아 페이징을 처리하는 메서드
     public ProductPagingDTO groupByCategory(SearchConditionListDTO searchConditionListDTO, Pageable pageable) {
         Page<Product> products = categoryRepository.groupByCategorySearch(searchConditionListDTO, pageable);
         List<ProductResponseDTO> responseDTOS = products.stream().map(ProductResponseDTO::new).collect(Collectors.toList());
@@ -31,5 +41,21 @@ public class ProductServiceImpl implements ProductService{
                 products.getTotalPages(),
                 products.getTotalElements(),
                 products.getSize());
+    }
+
+    //상품 클릭시 나오는 상품 detail 정보를 출력하는 메서드
+    @Override
+    public ProductDetailResponseDTO selectProductDetail(Long productId) {
+        Product product = productRepository.findProductFetchJoinById(productId).orElseThrow(NoSearchProductException::new);
+        return new ProductDetailResponseDTO().toDTO(product);
+    }
+
+    // 상품 클릭시 해당 상품의 최신리뷰 3개를 반환하는 메서드
+    @Override
+    public List<ReviewResponseDTO> selectProductDetailInReviews(Long productId, Pageable pageable) {
+        return reviewRepository.selectProductDetailReviews(productId, pageable)
+                .stream()
+                .map(ReviewResponseDTO::new)
+                .collect(Collectors.toList());
     }
 }
