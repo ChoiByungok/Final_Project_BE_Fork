3c87fffb476b444cf4a35e4f8673583519d860d2
CBO <quddhr9523@naver.com>
2023-03-30 01:17:05 +0900

    feat: 상품 상세조회 관련 repository 구현

--- src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductRepository.java
+++ src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductRepository.java
@@ -2,6 +2,18 @@ package com.fc.final7.domain.product.repository.datajpa;
 
 import com.fc.final7.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    @Query(value = "select p from Product p" +
+            " join fetch p.productPeriods pp" +
+            " join fetch p.productContents pc" +
+            " join fetch p.options po" +
+            " where p.id = :productId")
+    Optional<Product> findProductFetchJoinById(@Param("productId") Long productId);
 }
