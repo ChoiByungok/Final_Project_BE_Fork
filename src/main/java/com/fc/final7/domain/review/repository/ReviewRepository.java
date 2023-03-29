baebae339dd0939bf4cb6675206cf38622b38442
CBO <quddhr9523@naver.com>
2023-03-30 01:18:11 +0900

    feat: 상품 상세조회 했을시 상품에 연동된 후기 3개 반환하는 메서드 구현

--- src/main/java/com/fc/final7/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/fc/final7/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,19 @@
 package com.fc.final7.domain.review.repository;
 
-public interface ReviewRepository {
+import com.fc.final7.domain.review.entity.Review;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 상품 클릭시 해당 상품의 최신리뷰 3개를 반환하는 메서드
+    // pageable 인터페이스는 limit 3 을 걸기 위한 용도임
+    @Query(value = "select r from Review r" +
+            " where r.product.id = :productId" +
+            " order by r.createdDate desc")
+    List<Review> selectProductDetailReviews(@Param("productId") Long productId, Pageable pageable);
 }
