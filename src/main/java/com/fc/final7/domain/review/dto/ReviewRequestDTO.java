793c1722d21fc2c31e79979307b24fce13e88ee2
Hooni <lkh55913@naver.com>
2023-04-02 08:17:28 +0900

    feat #19: Review Request DTO 생성
    Review Request DTO 생성

---
+++ src/main/java/com/fc/final7/domain/review/dto/ReviewRequestDTO.java
@@ -0,0 +1,47 @@
+package com.fc.final7.domain.review.dto;
+
+import com.fc.final7.domain.product.entity.Product;
+import com.fc.final7.domain.review.entity.Review;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ReviewRequestDTO {
+    private int productId;
+    private String reservationCode;
+    private String reviewTitle;
+    private String name;
+    private String password;
+    private BigDecimal grade;
+
+    public static Review toEntity(ReviewRequestDTO reviewRequestDTO){
+        return Review.builder()
+                .product(Product.builder().id((long) reviewRequestDTO.getProductId()).build())
+                .title(reviewRequestDTO.getReviewTitle())
+                .grade(reviewRequestDTO.getGrade())
+                .thumbnail(null)
+                .name(reviewRequestDTO.getName())
+                .viewCount(0L)
+                .password(reviewRequestDTO.getPassword())
+                .build();
+    }
+
+    public static Review toUpdateEntity(ReviewRequestDTO reviewRequestDTO, Long id, String thumbnail){
+        return Review.builder()
+                .id(id)
+                .product(Product.builder().id((long) reviewRequestDTO.getProductId()).build())
+                .title(reviewRequestDTO.getReviewTitle())
+                .grade(reviewRequestDTO.getGrade())
+                .thumbnail(null)
+                .name(reviewRequestDTO.getName())
+                .viewCount(0L)
+                .password(reviewRequestDTO.getPassword())
+                .thumbnail(thumbnail)
+                .build();
+    }
+}
