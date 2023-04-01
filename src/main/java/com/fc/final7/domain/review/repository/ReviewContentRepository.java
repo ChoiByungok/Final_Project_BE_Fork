8ec190172141e78c9d16d7be6a253cbea09c3be3
Hooni <lkh55913@naver.com>
2023-04-02 08:14:57 +0900

    feat #19: Create JPA Repository
    Review 관련 Repository 생성
    Review Content 관련 Repository 생성

---
+++ src/main/java/com/fc/final7/domain/review/repository/ReviewContentRepository.java
@@ -0,0 +1,7 @@
+package com.fc.final7.domain.review.repository;
+
+import com.fc.final7.domain.review.entity.ReviewContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewContentRepository extends JpaRepository<ReviewContent, Integer> {
+}
