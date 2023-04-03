bd998e461695d2bd408900f20fd544bf8b246fd9
CBO <quddhr9523@naver.com>
2023-04-03 14:24:30 +0900

    refactor: 페이징이 빠지고 카테고리 목록도 반환 되도록 쿼리 수정

--- src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductRepository.java
+++ src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductRepository.java
@@ -1,12 +1,11 @@
 package com.fc.final7.domain.product.repository.datajpa;
 
 import com.fc.final7.domain.product.entity.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -19,8 +18,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             " where p.id = :productId")
     Optional<Product> findProductFetchJoinById(@Param("productId") Long productId);
 
-    @Query(value = "select p from Product p" +
+    @Query(value = "select distinct p from Product p" +
+            " join fetch p.categories pc" +
             " where p.title like %:keyWord%" +
             " or p.description like %:keyWord%")
-    Page<Product> searchProduct(@Param("keyWord") String keyWord, Pageable pageable);
+    List<Product> searchProduct(@Param("keyWord") String keyWord);
 }
