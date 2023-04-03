e875136041faa6965da4670ce5fd4605482e7c3c
Hooni <lkh55913@naver.com>
2023-04-03 16:14:15 +0900

    conf: Resolve CORS problem
    CORS 문제 해결

--- src/main/java/com/fc/final7/Final7Application.java
+++ src/main/java/com/fc/final7/Final7Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
 
@@ -22,4 +24,16 @@ public class Final7Application {
 		return new JPAQueryFactory(entityManager);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*");
+			}
+		};
+	}
 }
+
