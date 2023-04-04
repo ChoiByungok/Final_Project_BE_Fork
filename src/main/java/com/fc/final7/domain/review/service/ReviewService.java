67c2c9a188c128e645fa47f6f61edd23bf6f76a9
Hooni <lkh55913@naver.com>
2023-04-04 16:03:38 +0900

    feat #29: Review delete method

--- src/main/java/com/fc/final7/domain/review/service/ReviewService.java
+++ src/main/java/com/fc/final7/domain/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.fc.final7.domain.review.service;
 
 import com.fc.final7.domain.review.dto.ReviewPagingDTO;
 import com.fc.final7.domain.review.dto.ReviewRequestDTO;
+import com.fc.final7.domain.review.dto.ReviewResponseDTO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,4 +25,6 @@ public interface ReviewService {
                                    ReviewRequestDTO reviewRequestDTO,
                                    List<MultipartFile> multipartFileList,
                                    String text) throws IOException;
+
+    String deleteReview(int reviewId);
 }
