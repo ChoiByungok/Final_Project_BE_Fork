c1997ce1ea8cf77f222997bfcb127285c8c0023e
CBO <quddhr9523@naver.com>
2023-04-06 01:52:48 +0900

    feat: 예약기능 관련 컨트롤러 구현

--- src/main/java/com/fc/final7/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/fc/final7/domain/reservation/controller/ReservationController.java
@@ -1,9 +1,28 @@
 package com.fc.final7.domain.reservation.controller;
 
+import com.fc.final7.domain.reservation.dto.request.ReservationRequestDTO;
+import com.fc.final7.domain.reservation.service.ReservationService;
+import com.fc.final7.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RestController
 @RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    //예약 메서드
+    @PostMapping("/reservation")
+    public BaseResponse<String> createReservation (HttpServletRequest request, @RequestBody ReservationRequestDTO requestDTO) {
+        String header = request.getHeader(AUTHORIZATION);
+        String response = reservationService.createReservation(requestDTO, header);
+        return BaseResponse.of(1, "성공", response);
+    }
 }
