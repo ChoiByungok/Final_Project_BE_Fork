9f98fd1a11d496d7ededc94dba3cdbe200110dca
Hooni <lkh55913@naver.com>
2023-04-06 19:13:52 +0900

    feat #31: Create repository for comment
    댓글 관련 repository 작성

--- src/main/java/com/fc/final7/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/fc/final7/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,16 @@
 package com.fc.final7.domain.comment.repository;
 
-public interface CommentRepository {
+import com.fc.final7.domain.comment.entity.Comment;
+import com.fc.final7.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByReview(Review review);
+
+    Optional<Comment> findByIdAndPassword(Long id, String password);
+
 }
