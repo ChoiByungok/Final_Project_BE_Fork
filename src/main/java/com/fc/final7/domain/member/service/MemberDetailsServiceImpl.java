027607ad3a79ea3c7dd8fab0fd66c3677265fd6c
Seungjun Cha <chas369@naver.com>
2023-04-06 22:32:50 +0900

    #33 회원 정보 가져오는 코드 추가

--- src/main/java/com/fc/final7/domain/member/service/MemberDetailsServiceImpl.java
+++ src/main/java/com/fc/final7/domain/member/service/MemberDetailsServiceImpl.java
@@ -1,28 +1,29 @@
 package com.fc.final7.domain.member.service;
 
-import com.fc.final7.domain.member.adapter.MemberAdapter;
 import com.fc.final7.domain.member.entity.Member;
 import com.fc.final7.domain.member.repository.MemberRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityNotFoundException;
 
-@Service
+@Component
 @RequiredArgsConstructor
-@Getter
-public class MemberDetailService implements UserDetailsService {
-
+public class MemberDetailsServiceImpl implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Member member = memberRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
-        MemberAdapter memberAdapter = new MemberAdapter(member);
-        return memberAdapter;
+
+        if (member != null) {
+            MemberDetailImpl memberDetail = new MemberDetailImpl(member);
+            return memberDetail;
+        }
+
+        return null;
     }
 }
