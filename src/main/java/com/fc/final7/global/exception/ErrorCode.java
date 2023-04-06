50b36d171800f4a8aaf46bcb65de3616e96cbc29
Hooni <lkh55913@naver.com>
2023-04-06 22:18:37 +0900

    fix: Fix type mistake
    댓글 error  오타 수정

--- src/main/java/com/fc/final7/global/exception/ErrorCode.java
+++ src/main/java/com/fc/final7/global/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode{
     PRODUCT_NOT_FOUND(404, "PRODUCT-001", "존재하지 않는 상품입니다."),
     REVIEW_PASSWORD_MISMATCH(401, "REVIEW-001", "리뷰 비밀번호 불일치한 경우"),
     REVIEW_NOT_FOUND(404, "REVIEW-002", "리뷰가 없는 경우"),
-    COMMENT_PASSWORD_MISMATCH(401, "COMMENT-001", "후기 비밀번호 불일치한 경우");
+    COMMENT_PASSWORD_MISMATCH(401, "COMMENT-001", "댓글 비밀번호 불일치한 경우");
 
 
 
