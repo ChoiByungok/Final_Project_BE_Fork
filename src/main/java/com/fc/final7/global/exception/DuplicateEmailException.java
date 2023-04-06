15ac8660ec235a8ffdc37385d1eea4dd96ef4b7e
Seungjun Cha <chas369@naver.com>
2023-04-06 22:27:29 +0900

    #33 클래스 삭제

--- src/main/java/com/fc/final7/global/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.fc.final7.global.exception;
-
-public class DuplicateEmailException extends BusinessException{
-
-    public DuplicateEmailException() {
-        super("같은 이메일이 존재합니다", ErrorCode.DUPLICATE_ID);
-    }
-}
