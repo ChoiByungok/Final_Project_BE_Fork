67917faa73ffcc30763edb119d50d358a2bc7eb8
Seungjun Cha <chas369@naver.com>
2023-04-06 22:32:07 +0900

    #33 jwt 기능 및 필터 적용

--- src/main/java/com/fc/final7/global/security/SecurityConfig.java
+++ src/main/java/com/fc/final7/global/security/SecurityConfig.java
@@ -1,25 +1,50 @@
 package com.fc.final7.global.security;
 
+import com.fc.final7.domain.jwt.JwtProperties;
+import com.fc.final7.domain.jwt.JwtProvider;
+import com.fc.final7.domain.jwt.filter.JwtFilter;
+import com.fc.final7.domain.jwt.handler.JwtAccessDeniedHandler;
+import com.fc.final7.domain.jwt.handler.JwtAuthenticationEntryPoint;
+import com.fc.final7.domain.member.oauth.service.CustomOauth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig{
 
-    private String[] whiteList = {"/","/**", "/login", "/signUp"};
+    private String[] whiteList = {"/", "/**", "/login", "/signUp", "/signUp/checkId", "/sendEmail"};
+
+    private final CustomOauth2UserService customOauth2UserService;
+    private final JwtProperties jwtProperties;
+    private final JwtProvider jwtProvider;
+//    private final Oauth2SuccessHandler oauth2SuccessHandler;
+
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()); // 정적 리소스들
     }
 
     @Bean
@@ -30,11 +55,39 @@ public class SecurityConfig{
                 .formLogin().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
                 .and()
+                .exceptionHandling()
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+
+                .and()
+                .addFilterBefore(
+                        new JwtFilter(jwtProvider, jwtProperties),
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .authorizeRequests().antMatchers(
                         whiteList
                 ).permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+
+                // x-frame-options 공격 방어
+                .and()
+                .headers()
+                .frameOptions().sameOrigin()
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .oauth2Login()
+//                .successHandler(oauth2SuccessHandler)
+//                .loginProcessingUrl("")
+                .userInfoEndpoint()
+                .userService(customOauth2UserService);
+
+
 
         return http.build();
     }
