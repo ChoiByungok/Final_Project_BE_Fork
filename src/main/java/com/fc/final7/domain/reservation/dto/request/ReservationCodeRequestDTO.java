1067f648641059146fe5755b78aaf24a9387b71c
CBO <quddhr9523@naver.com>
2023-04-07 21:19:44 +0900

    feat: 예약 취소시 필요한 requestDTO 생성

---
+++ src/main/java/com/fc/final7/domain/reservation/dto/request/ReservationCodeRequestDTO.java
@@ -0,0 +1,14 @@
+package com.fc.final7.domain.reservation.dto.request;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class ReservationCodeRequestDTO {
+
+    private String reservationCode;
+}
