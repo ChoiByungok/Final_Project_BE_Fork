83647d5fbf7865c17b116e2fea1868966b528991
CBO <quddhr9523@naver.com>
2023-04-07 21:25:53 +0900

    feat: 예약 단건조회와 취소에 관한 메서드 구현 및jpql 작성

--- src/main/java/com/fc/final7/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/fc/final7/domain/reservation/repository/ReservationRepository.java
@@ -12,14 +12,22 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query(value = "select r from Reservation r" +
             " join fetch r.periods rp" +
-            " join fetch rp.productPeriod rppp" +
-            " join fetch rppp.product rppppp" +
+            " join fetch rp.productPeriod rpp" +
+            " join fetch rpp.product rppp" +
             " where r.member.id = :id")
     List<Reservation> selectReservations(@Param("id") Long id);
 
     @Query(value = "select r from Reservation r" +
             " join fetch r.periods rp" +
             " join fetch rp.productPeriod rpp" +
+            " join fetch rpp.product rppp" +
             " where r.id = :reservationId")
     Optional<Reservation> selectReservationDetailByReservationId(@Param("reservationId") Long reservationId);
+
+    @Query(value = "select r from Reservation r" +
+            " join fetch r.periods rp" +
+            " join fetch rp.productPeriod rpp" +
+            " join fetch rpp.product rppp" +
+            " where r.reservationCode = :reservationCode")
+    Optional<Reservation> findReservationByReservationCode(@Param("reservationCode") String reservationCode);
 }
