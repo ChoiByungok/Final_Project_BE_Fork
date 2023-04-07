de2e808dfef377e3c9b1fadc3f471f4429478a17
Seungjun Cha <chas369@naver.com>
2023-04-07 14:58:11 +0900

    #40 분리되어 있던 이메일 서비스 2개를 하나로 병합

--- src/main/java/com/fc/final7/domain/member/service/SmtpEmailService.java
+++ src/main/java/com/fc/final7/domain/member/service/SmtpEmailService.java
@@ -24,39 +24,38 @@ public class SmtpEmailService {
     private final PasswordEncoder encoder;
 
 
-    public MailDto createMail(String email, String phone)  { // 메일을 받는 대상의 이메일과 번호
+    public MailDto createMail(String email, String phone) throws MessagingException { // 메일을 받는 대상의 이메일과 번호
         String uuid = "";
         Member member = memberRepository.findByEmailAndPhone(email, phone).orElseThrow(EntityNotFoundException::new);
-
-        if (member != null){
-             uuid = resetPassword(member);
-        }
 
         MailDto mailDto = new MailDto();
         mailDto.setEmail(member.getEmail());
         mailDto.setTitle("goTogether 임시비밀번호 안내 이메일 입니다.");
-        mailDto.setMessage("goTogether 임시비밀번호 안내 이메일 입니다. 회원님의 임시 비밀번호는 "+ uuid + "입니다. 로그인 후 반드시 비밀번호를 변경해주세요");
-        return mailDto;
+        mailDto.setMessage("goTogether 임시비밀번호 안내 이메일 입니다. 회원님의 임시 비밀번호는 " + uuid + "입니다. 로그인 후 반드시 비밀번호를 변경해주세요");
+
+        if (member != null) {
+            uuid = resetPassword(member);
+
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, false); // false로 설정하면 단순 text메시지로 전달
+
+            messageHelper.setTo(mailDto.getEmail());
+            messageHelper.setSubject(mailDto.getTitle());
+            messageHelper.setText(mailDto.getMessage());
+            messageHelper.setFrom("chas369@naver.com");
+
+            javaMailSender.send(mimeMessage);
+            return mailDto;
+        }
+        return null;
     }
 
     @Transactional
-    public String resetPassword(Member member){
+    public String resetPassword(Member member) {
         String uuid = UUID.randomUUID().toString().substring(0, 8);
         member.updatePassword(encoder.encode(uuid));
         memberRepository.save(member);
         return uuid;
     }
 
-    public void sendTemporaryPasswordMail(MailDto mailDto) throws MessagingException {
-
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, false); // false로 설정하면 단순 text메시지로 전달
-
-        messageHelper.setTo(mailDto.getEmail());
-        messageHelper.setSubject(mailDto.getTitle());
-        messageHelper.setText(mailDto.getMessage());
-        messageHelper.setFrom("chas369@naver.com");
-
-        javaMailSender.send(mimeMessage);
-    }
 }
