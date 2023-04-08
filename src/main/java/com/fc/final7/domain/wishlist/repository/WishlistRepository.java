3a7214bb7c32d49975d0a05dce048055975ff41e
Hooni <lkh55913@naver.com>
2023-04-08 20:21:29 +0900

    feat #39: Create method for deleteall wishlist in repository

--- src/main/java/com/fc/final7/domain/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/fc/final7/domain/wishlist/repository/WishlistRepository.java
@@ -4,12 +4,13 @@ import com.fc.final7.domain.member.entity.Member;
 import com.fc.final7.domain.product.entity.Product;
 import com.fc.final7.domain.wishlist.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
     Optional<Wishlist> findByMemberAndProduct(Member member, Product Product);
+
+    void deleteAllByMember(Member member);
 
 }
