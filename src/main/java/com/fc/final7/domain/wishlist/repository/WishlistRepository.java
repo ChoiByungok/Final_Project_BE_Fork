bcc22ca37a8f8b5c6d406736f1947041e99d4a60
Hooni <lkh55913@naver.com>
2023-04-08 19:32:14 +0900

    feat #39: Create method find by member and password

--- src/main/java/com/fc/final7/domain/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/fc/final7/domain/wishlist/repository/WishlistRepository.java
@@ -1,9 +1,16 @@
 package com.fc.final7.domain.wishlist.repository;
 
-
+import com.fc.final7.domain.member.entity.Member;
+import com.fc.final7.domain.product.entity.Product;
 import com.fc.final7.domain.wishlist.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+//    @Query("select w from wishlist w where w.")
+    Optional<Wishlist> findByMemberAndProduct(Member member, Product Product);
 
 }
