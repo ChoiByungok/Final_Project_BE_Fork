26c06751d3b10918783b170a0565bf52ad2eb1c9
CBO <quddhr9523@naver.com>
2023-04-08 23:30:40 +0900

    feat: 해당 상품의 옵션을 불러오는 메서드 구현

--- src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductOptionRepository.java
+++ src/main/java/com/fc/final7/domain/product/repository/datajpa/ProductOptionRepository.java
@@ -1,7 +1,11 @@
 package com.fc.final7.domain.product.repository.datajpa;
 
+import com.fc.final7.domain.product.entity.Product;
 import com.fc.final7.domain.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+    List<ProductOption> findAllByProduct(Product product);
 }
